c325975e4af840216330a12ec9d2d5ed20043c95
Koushal Saini <sainikoushal@gmail.com>
2025-10-23 10:44:10 +0000

    pre req scripts

---
+++ script_s/01_preqs.bash
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install net-tools tree zip unzip bash-completion -y
+mkdir ~/_script_installs
+cd ~/_script_installs
+# Install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+echo "complete -C '/usr/local/bin/aws_completer' aws" >> ~/.bashrc
+
+# Install Terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt-get install terraform -y
+
+terraform -install-autocomplete
+#
+# Install EKSCTL
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+
+echo ". <(eksctl completion bash)" >> ~/.bashrc
+#
+Install KubeCTL
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
+
+echo 'source <(kubectl completion bash)' >> ~/.bashrc
+#
+
+# Install Node
+curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash - ;
+sudo apt-get install -y nodejs
+corepack prepare yarn@stable --activate
+
+# at END
+source ~/.bashrc
